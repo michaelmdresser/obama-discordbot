8bb079af230beda0bf60357e7ea678b63a742742
Michael Dresser <michaelmdresser@gmail.com>
2016-11-19 18:00:41 -0700

    cleanup with function

--- testbot.js
+++ testbot.js
@@ -2,6 +2,12 @@ var Discord = require("discord.js");
 var request = require("request");
 var bot = new Discord.Client();
 
+function requestContent(size, cb) {
+    request("https://talk-to-obama.herokuapp.com/chat?size=" + size, function(error, response, body) {
+        return cb(JSON.parse(body).content)
+    });
+}
+
 bot.on("message", msg => {
 
     let prefix = "!";
@@ -13,26 +19,40 @@ bot.on("message", msg => {
 
 
     else if (msg.content.startsWith(prefix + "obama tweet")) {
-        request("https://talk-to-obama.herokuapp.com/chat", function(error, response, body) {
-            msg.channel.sendMessage(JSON.parse(body).content)
+        //request("https://talk-to-obama.herokuapp.com/chat", function(error, response, body) {
+        //    msg.channel.sendMessage(JSON.parse(body).content)
+        //});
+        requestContent("tweet", content => {
+            msg.channel.sendMessage(content)
         });
     }
 
     else if (msg.content.length > 7) {
         if (Number(msg.content.substring(7, msg.content.length)) != NaN) {
-            request("https://talk-to-obama.herokuapp.com/chat?size=" + Number(msg.content.substring(7, msg.content.length)), function(error, response, body) {
-                msg.channel.sendMessage(JSON.parse(body).content)
+            //request("https://talk-to-obama.herokuapp.com/chat?size=" + Number(msg.content.substring(7, msg.content.length)), function(error, response, body) {
+            //    msg.channel.sendMessage(JSON.parse(body).content)
+            //});
+            //msg.channel.sendMessage(requestContent(Number(msg.content.substring(7, msg.content.length))));
+            requestContent(Number(msg.content.substring(7, msg.content.length)), content => {
+                msg.channel.sendMessage(content)
             });
         } else {
-            request("https://talk-to-obama.herokuapp.com/chat", function(error, response, body) {
-                msg.channel.sendMessage(JSON.parse(body).content)
+            //request("https://talk-to-obama.herokuapp.com/chat", function(error, response, body) {
+            //    msg.channel.sendMessage(JSON.parse(body).content)
+            //});
+            requestContent("", content => {
+                msg.channel.sendMessage(content)
             });
+
         }
     }
 
     else if (msg.content.startsWith(prefix + "obama")) {
-        request("https://talk-to-obama.herokuapp.com/chat", function(error, response, body) {
-            msg.channel.sendMessage(JSON.parse(body).content)
+        //request("https://talk-to-obama.herokuapp.com/chat", function(error, response, body) {
+        //    msg.channel.sendMessage(JSON.parse(body).content)
+        //});
+        requestContent("", content => {
+            msg.channel.sendMessage(content)
         });
     }
 
